d5f8510985e225b9778e8bb6781cffb239996799
shaikhrafi28 <shaikhrafi08@gmail.com>
2019-02-18 19:30:15 +0530

    w

--- src/test/java/com/outlook/qa/testcases/LoginPage/LoginTest01.java
+++ src/test/java/com/outlook/qa/testcases/LoginPage/LoginTest01.java
@@ -10,13 +10,13 @@ import com.outlook.qa.pages.MailBoxPage;
 import com.outlook.qa.pages.LoginPage;
 import com.outlook.qa.util.extentreports.ExtentTestManager;
 
-public class Test01 extends TestBaseSetup {
+public class LoginTest01 extends TestBaseSetup {
 
 	// Extra information:
 
 	// 2) We send these driver and wait variables to the page class with below
 	// declaration
-	// Homepage homepage = new HomePage(driver,wait);
+	
 	// 3) super () method in page class transfer the driver and wait variables
 	// values to the BasePage class.
 	@BeforeMethod
@@ -32,37 +32,35 @@ public class Test01 extends TestBaseSetup {
 
 		// *************PAGE INSTANTIATIONS*************
 		LoginPage signupPage = new LoginPage();
-	//	MailBoxPage mailboxpage = new MailBoxPage();
+		// MailBoxPage mailboxpage = new MailBoxPage();
 
 		// *************PAGE METHODS********************
 		signupPage.signInUser("shaikhrafi08@hotmail.com", "Helloworld");
-		
+
 		// *************ASSERTIONS***********************
 		signupPage.verifyLoginPassword();
 	}
 
-	/*@Test(enabled=false,priority = 1, description = "Invalid Login Scenario with empty username and password.")
-	public void invalidLoginTest_EmptyUserEmptyPassword() throws InterruptedException {
-		// ExtentReports Description
-		ExtentTestManager.getTest().setDescription("Invalid Login Scenario with empty username and password.");
-
-		// *************PAGE INSTANTIATIONS*************
-		SignUpPage signupPage = new SignUpPage();
-		MailBoxPage mailboxpage = new MailBoxPage();
-
-		// *************PAGE METHODS********************
-		signupPage.signInUser(null, null);
-		mailboxpage = new MailBoxPage();
-		mailboxpage.clicknewmail();
-
-		// *************ASSERTIONS***********************
-		Thread.sleep(500);
-		
-	}*/
+	/*
+	 * @Test(enabled=false,priority = 1, description =
+	 * "Invalid Login Scenario with empty username and password.") public void
+	 * invalidLoginTest_EmptyUserEmptyPassword() throws InterruptedException { //
+	 * ExtentReports Description ExtentTestManager.getTest().
+	 * setDescription("Invalid Login Scenario with empty username and password.");
+	 * 
+	 * // *************PAGE INSTANTIATIONS************* SignUpPage signupPage = new
+	 * SignUpPage(); MailBoxPage mailboxpage = new MailBoxPage();
+	 * 
+	 * // *************PAGE METHODS******************** signupPage.signInUser(null,
+	 * null); mailboxpage = new MailBoxPage(); mailboxpage.clicknewmail();
+	 * 
+	 * // *************ASSERTIONS*********************** Thread.sleep(500);
+	 * 
+	 * }
+	 */
 
 	@AfterMethod()
 	public void cteardown() {
-
 		driver.quit();
 
 	}
