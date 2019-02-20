36482ea0d90d0df7fc2141c4819727bf19c9703a
shaikhrafi08 <33877774+shaikhrafi08@users.noreply.github.com>
2019-02-21 03:19:34 +0530

    bat file

--- run.bat
+++ run.bat
@@ -1,1 +1,7 @@
-java -cp bin;libs/* org.testng.TestNG testng2.xml 
+set ProjectPath=C:\Users\sys\git\DemoProjectOutlook_TestNG
+set classpath=C:\Users\sys\.m2\repository\org\testng\testng\6.3.1\testng-6.3.1.jar
+pause
+java -cp %classpath% org.testng.TestNG %ProjectPath%\testng2.xml
+
+
+
