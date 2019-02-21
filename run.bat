5cdd6ab1b624bc056cd31a3a0a35fd5a552f2933
shaikhrafi08 <33877774+shaikhrafi08@users.noreply.github.com>
2019-02-21 23:04:52 +0530

    qw

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
-set ProjectPath=C:\Users\sys\git\DemoProjectOutlook_TestNG
-set classpath=C:\Users\sys\.m2\repository\org\testng\testng\6.14.3
-pause
-java -cp %classpath% org.testng.TestNG testng2.xml
+call mvn clean
+call mvn test
+call mvn package
 
 
 
